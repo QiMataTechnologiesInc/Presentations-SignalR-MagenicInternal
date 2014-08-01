2bf932e3ed9ef2af9413f0439dc4bd24d475a667
Jared Rhodes <jrhodes@qimata.com>
2014-08-01 16:49:00 -0400

    Initial Commit

---
+++ Magenic.SignalR/Hubs/BlockHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Magenic.SignalR.Hubs
+{
+    public class BlockHub : Hub
+    {
+        public void ImageMoved(int x, int y)
+        {
+            Clients.Others.MoveImage(x, y);
+        }
+    }
+}
---
+++ Magenic.SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Magenic.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.Others.NewMessage(message);
+        }
+    }
+}
